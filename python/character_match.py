abab6e00f072fe815d8e11c89b275f5d91a51b81
Evan Garcia <evanag116@yahoo.com>
2022-05-24 19:36:44 -0500

    actually working now

--- python/character_match.py
+++ python/character_match.py
@@ -4,10 +4,12 @@ from operator import is_
 
 
 def is_character_match(string1, string2):
+	string1 = string1.lower()
+	string1 = string1.replace(" ", "")
+	string2 = string2.replace(" ", "")
+	string2 = string2.lower()
+	x = {n: string1.count(n) for n in string1}
+	y = {n: string2.count(n) for n in string2}
 	
-    x = {n: string1.count(n) for n in string1}
-    y = {n: string2.count(n) for n in string2}
-  
-
-    return x == y
+	return x == y
 
