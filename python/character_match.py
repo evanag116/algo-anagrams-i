61f96f441ab71381f9381d79eb7d81eb2c2d1d03
Evan Garcia <evanag116@yahoo.com>
2022-05-24 19:27:59 -0500

    py completed

---
+++ python/character_match.py
@@ -0,0 +1,14 @@
+# Don't forget to run your tests!
+
+from operator import is_
+
+
+def is_character_match(string1, string2):
+	
+    x = {n: string1.count(n) for n in string1}
+    y = {n: string2.count(n) for n in string2}
+  
+
+    return x == y
+
+print(is_character_match("anagram", "nagaram"))
