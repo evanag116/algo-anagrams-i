eab953a2507e8df66fa4e04323093eb93f3c69b8
Evan Garcia <evanag116@yahoo.com>
2022-05-26 10:27:45 -0500

    housekeeping items

--- python/character_match.py
+++ python/character_match.py
@@ -9,8 +9,8 @@ def is_character_match(string1, string2):
 	string1, string2 = string1.replace(" ", ""), string2.replace(" ", "")
 
 	
-	x = {n: string1.lower().replace(" ", "").count(n) for n in string1.replace(" ", "").lower()}
-	y = {n: string2.lower().replace(" ", "").count(n) for n in string2.replace(" ", "").lower()}
+	x = {n: string1.count(n) for n in string1}
+	y = {n: string2.count(n) for n in string2}
 
 	return x == y
 
