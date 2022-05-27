469875c4a868eeaf934e65c50671c3f572500545
Evan Garcia <evanag116@yahoo.com>
2022-05-26 20:21:22 -0500

    completed in js

--- js/characterMatch.js
+++ js/characterMatch.js
@@ -1,3 +1,18 @@
 exports.isCharacterMatch = function(string1, string2) {
 
+    string1 = string1.toLowerCase().replace(/ /g, "")
+    string2 = string2.toLowerCase().replace(/ /g, "")
+
+    string1 = string1.split("")
+    string2 = string2.split("")
+
+    string1 = string1.sort()
+    string2 = string2.sort()
+
+    string1 = string1.join("")
+    string2 = string2.join("")
+
+    return string1 === string2
 };
+
+
--- python/character_match.py
+++ python/character_match.py
@@ -14,8 +14,6 @@ def is_character_match(string1, string2):
 
 	return x == y
 
-	
-
 	# solved in one line
 	# return {n: string1.lower().replace(" ", "").count(n) for n in string1.replace(" ", "").lower()} == {n: string2.lower().replace(" ", "").count(n) for n in string2.replace(" ", "").lower()}
 
